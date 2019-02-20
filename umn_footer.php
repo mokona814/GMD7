afabd1e9efa6e9cf8edfef0c47a929552ac53ddb
mokona814 <37744072+mokona814@users.noreply.github.com>
2019-02-20 09:11:32 -0600

    Add files via upload

---
+++ umn_footer.php
@@ -0,0 +1,32 @@
+
+
+	
+			<!-- UMN footer-->
+	<section id="umnhf-uf" class="umnhf">
+		<div class="umnhf-uf-sub">
+		<h2 class="visually-hidden">Contact Information</h2>
+		<address id="umnhf-uf-ci" class="umnhf">
+			<p class="umnhf-f-title">Goldstein Museum of Design</p>
+			<p>364 McNeal Hall,  1985 Buford Ave</br> St. Paul, MN, 55108</p>				<p><abbr title="Phone Number">P</abbr>: <a href="tel:6126247434">612-624-7434</a> I <a href="mailto:gmd@umn.edu">gmd@umn.edu</a></p>		</address>
+		<!-- OPTIONAL SOCIAL MEDIA LINKS -->
+			<section id="umnhf-uf-sm" class="umnhf">
+				<h2 class="visually-hidden">Connect on Social Media</h2>
+				<ul>
+					<li class="facebook"><a href="https://www.facebook.com/GoldsteinMuseumofDesign/"><span class="visually-hidden">Facebook</span></a></li><li class="twitter"><a href="https://twitter.com/GoldsteinMuseum"><span class="visually-hidden">Twitter</span></a></li>
+					<li class="instagram"><a href="https://www.instagram.com/goldstein_museum/"><span class="visually-hidden">Instagram</span></a></li>
+				</ul>
+			</section>			</div>
+				</section>
+	<!-- END OPTIONAL UNIT FOOTER -->
+	<!-- BEGIN UMN FOOTER -->
+	<footer id="umnhf-f" class="umnhf" role="contentinfo">
+		<nav id="umnhf-f-myu">
+			<h3 class="umnhf-f-title visually-hidden">For Students, Faculty, and Staff</h3>
+			<ul>
+				<li><a href="http://onestop.umn.edu/">One Stop</a></li>
+				<li><a href="https://www.myu.umn.edu/">My U <span></span></a></li>
+			</ul>
+		</nav>
+		<small>&copy; <span id="cdate">2018</span> Regents of the University of Minnesota. All rights reserved. The University of Minnesota is an equal opportunity educator and employer. <a href="http://privacy.umn.edu">Privacy Statement</a></small>
+	</footer>
+	<!-- END UofM FOOTER -->
